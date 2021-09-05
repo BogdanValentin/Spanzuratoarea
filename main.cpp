aa4e2b44437615273d74edff776e2f2e934351eb
Bogdan Valentin <63101897+BogdanValentin@users.noreply.github.com>
2021-09-05 19:43:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -124,54 +124,68 @@ void desene(int greseli) // desene pentru fiecare numar de greseli. (as putea si
 int main()
 {
     int greseli, aparitii, counter = 0;
-    unsigned int i; // am declarat unsigned pt ca apareau niste avertismente.
-    char cuvant[50], cuvantascuns[50], litera, stopper;
+    unsigned int i;                                                 // am declarat unsigned pt ca apareau niste avertismente.
+    char cuvant[50], cuvantascuns[50], litera, stopper, literegresite[50];
 
     do
     {
-        if (counter > 0) // de cate ori am jucat pana acum (de cand am deschis consola).
+        if (counter > 0)                                            // de cate ori am jucat pana acum (de cand am deschis consola).
         {
             for(i = 0; i < 50; i++)
-                cuvantascuns[i]='\0'; // dam clear la cuvantascuns.
+                cuvantascuns[i]='\0';                               // dam clear la cuvantascuns.
             for(i = 1; i <= 15; i++)
-                cout<<"\n\n\n\n\n"; // dam clear la consola.
+                cout<<"\n\n\n\n\n";                                 // dam clear la consola.
         }
 
         cout<<"Bun venit la jocul Spanzuratoarea!\nScrieti cuvantul: \n";
         cin.getline(cuvant, 50);
         for(i = 0; i < strlen(cuvant); i++)
-            if(cuvant[i] < 97)                 // daca litera de pe poz i este majuscula
-                cuvant[i] = cuvant[i] + 32; // o facem mica pentru a fi comparata mai usor.
-
-        for(i = 0; i < strlen(cuvant); i++)
         {
-            cuvantascuns[i] = '*'; //cuvantul ascuns va fi de forma "****** **** ****".
+            cuvantascuns[i] = '*';                                  //cuvantul ascuns va fi de forma "****** **** ****".
+            if(cuvant[i] >= 65 && cuvant[i] <= 90 )                 // daca litera de pe poz i este majuscula
+                cuvant[i] = cuvant[i] + 32;                         // o facem mica pentru a fi comparata mai usor.
             if(cuvant[i] == ' ')
-                cuvantascuns[i] = ' '; // atentie ca spatiile sunt luate ca atare, nu drept caractere ce trebuie ghicite.
+                cuvantascuns[i] = ' ';                              // atentie ca spatiile sunt luate ca atare, nu drept caractere ce trebuie ghicite.
         }
-        cuvantascuns[strlen(cuvant)]='\0'; //ultimul caracter este adaugat fortat pentru a delimita sirul (apar buguri ciudate fara acest pas cum ar fi simboluri nonsense).
 
-        greseli = 0; // contor ce ne ajuta la stabilirea desenului si cand sa ne oprim (la 7 greseli este spanzurat).
-        while(greseli <= 6 && strchr(cuvantascuns,'*') != NULL) // daca avem mai putin de 7 greseli si inca mai sunt caractere necunoscute continuam.
+        cuvantascuns[strlen(cuvant)]='\0';                          //ultimul caracter este adaugat fortat pentru a delimita sirul (apar buguri ciudate fara acest pas cum ar fi simboluri nonsense).
+
+        greseli = 0;                                                // contor ce ne ajuta la stabilirea desenului si cand sa ne oprim (la 7 greseli este spanzurat).
+
+        while(greseli <= 6 && strchr(cuvantascuns,'*') != NULL)     // daca avem mai putin de 7 greseli si inca mai sunt caractere necunoscute continuam.
         {
             for(i = 1; i <= 15; i++)
-                cout<<"\n\n\n\n\n"; // dam clear la consola.
-            desene(greseli); // afisam desen
+                cout<<"\n\n\n\n\n";                                 // dam clear la consola.
+            desene(greseli);                                        // afisam desen
             cout<<endl;
             cout<<cuvantascuns<<endl;
+            if(greseli > 0)
+            {
+                cout<<"Litere gresite: ";
+                for(int i = 1; i <= greseli; i++)
+                    cout<<literegresite[i]<<" ";
+            }
+
+            cout<<endl;
+
             cout<<"Litera dvs.:";
-            cin>>litera; // citim litera ghicita.
+            cin>>litera;                                            // citim litera ghicita.
             aparitii = 0;
-            for(i = 0;i < strlen(cuvant); i++)
+            for(unsigned int i = 0;i < strlen(cuvant); i++)
                 if(cuvant[i] == litera)
                 {
                     aparitii++;
-                    cuvantascuns[i] = litera; // daca litera apare in poz i o adaugam in cuvantascuns.
+                    cuvantascuns[i] = litera;                       // daca litera apare in poz i o adaugam in cuvantascuns.
                 }
-            if(aparitii == 0) // daca litera ghicita nu apare niciodata inseamna ca e gresita.
+            if(aparitii == 0)                                       // daca litera ghicita nu apare niciodata inseamna ca e gresita.
+            {
                 greseli++;
+                literegresite[greseli] = litera;
+            }
+
         }
-        if(greseli == 7) // daca pierdem jocul aratam desenul final si ce cuvant era.
+
+        if(greseli == 7)                                            // daca pierdem jocul aratam desenul final si ce cuvant era.
         {
             for(i = 1; i <= 15; i++)
                 cout<<"\n\n\n\n\n";
@@ -179,7 +193,7 @@ int main()
             cout<<endl;
             cout<<"Cuvantul era "<<cuvant<<"."<<endl;
         }
-        else // daca castigam jocul aratam desenul la care am ramas si ce cuvant era
+        else                                                        // daca castigam jocul aratam desenul la care am ramas si ce cuvant era
         {
             for(i = 1; i <= 15; i++)
             cout<<"\n\n\n\n\n";
@@ -188,20 +202,21 @@ int main()
             cout<<"Cuvantul era "<<cuvant<<"."<<endl;
         }
 
-        cout<<"Doriti sa jucati in continuare? (Y/N)\n"; // acceptam o litera drept raspuns.
-        cin>>stopper;                                    // daca litera nu este Y,y,N sau n atunci eroare si recitim.
-        while(stopper != 'Y' && stopper && 'y' && stopper != 'N' && stopper != 'n')
+        cout<<"Doriti sa jucati in continuare? (Y/N)\n";            // acceptam o litera drept raspuns.
+        cin>>stopper;                                               // daca litera nu este Y,y,N sau n atunci eroare si recitim.
+        while(stopper != 'Y' && stopper != 'y' && stopper != 'N' && stopper != 'n')
         {
             cout<<"Eroare\n";
             cout<<"Doriti sa jucati in continuare? (Y/N)\n";
             cin>>stopper;
         }
-        if(counter == 0) // nu are rost sa contorizam daca jucam mai mult de 2 ori, testul e doar pt a da clear la consola
+        if(counter == 0)                                            // nu are rost sa contorizam daca jucam mai mult de 2 ori, testul e doar pt a da clear la consola
             counter++;
 
-        cin.ignore(); // necesar pt a citi cu getline mai sus
-    }while(stopper == 'y' || stopper == 'Y'); // daca stopper este y sau Y reluam jocul.
+        cin.ignore();                                               // necesar pt a citi cu getline mai sus
+    }while(stopper == 'y' || stopper == 'Y');                       // daca stopper este y sau Y reluam jocul.
+
     cout<<endl<<"Merci de joc :D"<<endl;
+
     return 0;
 }
-
